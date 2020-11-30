3c07ebbccb916f7fa2ddb223844548545cebf44c
Anna Grzywa <grzywa.anna90@gmail.com>
2020-11-30 14:29:21 +0100

    Fix html, css and js files, delete close button, prev and next buttons. Change modal to regular div

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
-const myModal = document.querySelector('.modal');
-const photos = document.querySelectorAll('.gallery__smallImage')
-const closeModalBtn = document.querySelector('.modal__close-btn');
-const prevImgBtn = document.querySelector('#prevButton');
-const nextImgBtn = document.querySelector('#nextButton');
+const lightbox = document.querySelector('.lightbox');
+const gallery = document.querySelector('.gallery');
+gallery.appendChild(lightbox);
+const images = document.querySelectorAll('.gallery__smallImage');
 
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img');
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+    console.log(lightbox);
+  })
+})
 
-// click close button to close modal
-closeModalBtn.addEventListener('click', () => {
-    myModal.style.display = 'none';
-});
-
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
